13331b17fa51e4ef9b0cf45446bddc4e7e27f455
yeseulbi <ekman860@gmail.com>
2026-01-23 22:54:28 +0900

    add-monster-ai-CombatType, State

--- Assets/Chracter/Monster/Scripts/MonsterMovement.cs
+++ Assets/Chracter/Monster/Scripts/MonsterMovement.cs
@@ -1,11 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public abstract class MonsterMovement : MonoBehaviour
 {
-    protected MonsterState myStat;
+    LayerMask targetLayer = LayerMask.NameToLayer("Player");
+    protected MonsterState currentState;
+    protected State currentTarget;
+    WaitForSeconds setTargetWaitTime = new WaitForSeconds(0.2f);
+    protected MonsterType myType => currentState.monsterType;
     private void Awake()
     {
-        myStat = GetComponent<MonsterState>();
+        currentState = GetComponent<MonsterState>();
+        StartCoroutine(SetTargetCoroutine());
     }
     void Start()
     {
@@ -14,6 +20,61 @@ public abstract class MonsterMovement : MonoBehaviour
 
     void Update()
     {
-        
+        if (currentState.isDead)
+            return;
+
+        if (myType.isPatrol)
+        {
+            PatrolMove();
+            return;
+        }
+
+        if (currentTarget == null)
+            return;
+
+        MoveToTarget();
     }
+    public void Attack()
+    {
+        currentTarget.TakeDamage(myType.attackDamage);
+
+        if (currentTarget.isDead)
+            StartCoroutine(SetTargetCoroutine());
+    }
+    public void MoveToTarget()
+    { 
+
+    }
+    IEnumerator SetTargetCoroutine()
+    {
+        while (!currentState.isDead && currentTarget != null)
+        {
+            yield return setTargetWaitTime;
+            SetTarget();
+        }
+    }
+    void SetTarget()
+    {
+        Collider[] entitys = Physics.OverlapSphere(transform.position, myType.attackRange, targetLayer);
+        float closestDistSqr = Mathf.Infinity;
+        Transform targetTrans = null;
+
+        foreach (var t in entitys)
+        {
+            float dSqr = (t.transform.position - transform.position).sqrMagnitude;
+
+            if (dSqr < closestDistSqr)
+            {
+                closestDistSqr = dSqr;
+                targetTrans = t.transform;
+            }
+        }
+        if (targetTrans != null)
+            currentTarget = targetTrans.GetComponent<State>();
+    }
+    void PatrolMove()
+    {
+
+    }
+    public abstract void OnHitReaction();
 }
--- Assets/Chracter/Monster/Scripts/MonsterState.cs
+++ Assets/Chracter/Monster/Scripts/MonsterState.cs
@@ -1,17 +1,22 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-public class MonsterState : MonoBehaviour
+public class MonsterState : State
 {
-    MonsterType myType;
+    MonsterMovement movement => GetComponent<MonsterMovement>();
+    public MonsterType monsterType => (MonsterType)myType;
     CombatStat myCombatStat;
 
-    float currentHp;
-    private void Awake()
+    public override void Awake()
     {
-        currentHp = myType.maxHp;
-        myCombatStat = MonsterManager.inst.CombatStatList[(int)myType.combatType];
+        base.Awake();
+        myCombatStat = MonsterManager.inst.CombatStatList[(int)monsterType.combatType];
     }
-    public bool isDie() =>
-        currentHp >= 0;
+    public override void Dead()
+    {
+        throw new System.NotImplementedException();
+    }
+    public override void TakeDamage(float damage)
+    {
+        base.TakeDamage(damage);
+
+        movement.OnHitReaction();
+    }
 }
